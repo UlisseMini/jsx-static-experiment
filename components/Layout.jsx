4a3c17f1bc0c970430320f4d8f3f9cc9e18c98d7
Ulisse Mini <ulisse.mini@gmail.com>
2020-12-11 16:26:16 -0500

    Add layout basic component

---
+++ components/Layout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const defaultTitle = 'Uli'
+
+export default function Layout({ title, children }) {
+	return (
+	<html>
+		<head>
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<link rel="stylesheet" href="/styles.css" />
+			<title>{ title || defaultTitle }</title>
+		</head>
+
+		<body>
+			<header>
+				<a href="/">
+					<img src="/profile.png" alt="cute seal" className="round" />
+				</a>
+				<h1>About</h1>
+			</header>
+			<main>
+				{children}
+			</main>
+		</body>
+
+	</html>
+	)
+}
