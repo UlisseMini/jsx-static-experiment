219083bee10ce56986f6eeaf9cc28f33b841c06d
Ulisse Mini <ulisse.mini@gmail.com>
2020-12-11 17:58:45 -0500

    style: standard --fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout.jsx'
 export default function Index () {
   return (
     <Layout>
-      <section id="about">
+      <section id='about'>
         <h1>About</h1>
         <p>
           I'm Ulisse Mini, I'm 16 and an aspring software developer, AI researcher and entrepreneur.
@@ -14,7 +14,7 @@ export default function Index () {
           I'm currently teaching myself linear algebra, multivariable calculus and probability theory.
           <br />
           At the same time I'm trying to improve my programming a bit,
-          I plan to make interactive AI demonstrations on this site, things like <a href="https://blog.otoro.net/2015/05/13/neural-slime-volleyball-evolution-demo/">slime volleyball</a> 
+          I plan to make interactive AI demonstrations on this site, things like <a href='https://blog.otoro.net/2015/05/13/neural-slime-volleyball-evolution-demo/'>slime volleyball</a>
 
         </p>
         <p>
@@ -22,7 +22,7 @@ export default function Index () {
         </p>
       </section>
 
-      <section id="posts">
+      <section id='posts'>
         <h1>Posts</h1>
         <i>No posts yet</i>
       </section>
