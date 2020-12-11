72e2ff121c6d659f922873651037daee26cb410e
Ulisse Mini <ulisse.mini@gmail.com>
2020-12-11 16:20:58 -0500

    Remove html comment

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Layout from '../components/Layout.jsx'
 
 export default function Index() {
 	return (
-	<>
-		<h1>This is the Index!</h1>
-		<a href="/foo.html">foo</a>
-	</>
-	)
+	<Layout>
+		<p>
+			I'm Ulisse Mini, I'm 16 and an aspring software developer, AI researcher and entrepreneur.
+		</p>
+
+		<h1>Posts</h1>
+		<div>
+			<i>Nothing yet ;)</i>
+		</div>
+	</Layout>
+			)
 }
