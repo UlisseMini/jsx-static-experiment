ef665dc7c4b7e53d890e0c902361fd0f9623b6f6
Ulisse Mini <ulisse.mini@gmail.com>
2020-12-11 20:30:47 -0500

    Add icon

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,6 +6,7 @@ export default function Head ({ children }) {
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <link rel='stylesheet' href='/styles.css' />
+      <link rel='icon' href='/profile.png' />
       {children}
     </head>
   )
